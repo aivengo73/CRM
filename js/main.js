cbdc79a857736648eef2781bbf59a5505cf955c7
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2023-10-12 13:23:03 +0300

    Добавлен файл js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const productName = 'Banana';
+const productСont = 100;
+const productCategory = 'foods';
+const productPrice = 70;
+
+console.log( productName );
+
+const sum = productСont * productPrice;
+
+console.log( sum );
+
+ 
