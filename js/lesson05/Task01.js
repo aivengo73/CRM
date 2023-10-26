8d98c3711284ab1cb6feea247efcafc3740a4e58
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2023-10-26 11:18:05 +0300

    Модифицированы файлы Task01, Task02, Task03, добавил коментарии

--- js/lesson05/Task01.js
+++ js/lesson05/Task01.js
@@ -19,3 +19,20 @@ function pow() {
 	}
 }
  pow();
+
+ 
+//  function pow() {
+
+// 	let table = [];
+
+// 	for (let i = 1; i <= 10; i++) {
+// 			let row = {};
+
+// 			for (let j = 1; j <= 10; j++) {
+// 					row[j] = j ** i;
+// 			}
+// 			table.push(row);
+// 	}
+// 	console.table(table);
+// }
+// pow();
