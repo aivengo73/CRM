36f9030cdaa1363d2380c07391b7c632ccfd0fb1
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2023-10-31 13:24:35 +0300

    Промежуточный коммит с исправлениями

--- js/lesson06/Task04.js
+++ js/lesson06/Task04.js
@@ -19,7 +19,6 @@
 // В каждом массиве из двух элементов, первым является количество товаров в чеке, а вторым — общая сумма.
 // Необходимо посчитать среднюю стоимость одного товара в магазине.
 
-
 const allCashbox = [
 	[12, 4500],
 	[7, 3210],
@@ -32,30 +31,29 @@ const allCashbox = [
 ];
 
 /* Переиндексируем массив и получим упрощённый вариант массива */
-
 let array = allCashbox.reduce((accum, item, i) => {
-	accum.push(allCashbox[i][1]);
-
+	accum.push(allCashbox[i][1] * allCashbox[i][0]);
 	return accum
 }, []);
-
-/* В результате получим новый массив array */
-
-console.log(array); /* [4500, 3210, 650, 1250, 7830, 990, 13900, 370] */
-
+console.log(array); /* [54000, 22470, 2600, 3750, 70470, 990, 83400, 370] */
 
 /* Посчитаем среднюю стоимость одного товара в магазине. */
-
 function getAveragePriceGoods() {
 	let sum = 0;
-	let item = 0;
+	let items = 0;
+	let num = [];
+/* общая стоимость товаров */
 	for (let i = 0; i < array.length; i++) {
-		item++;
-		sum = sum + array[i];
+		sum = sum + array[i];                       // 238050
 	}
-	console.log(sum); // 32700 - общая сумма  товаров
-	sum = sum / item;
+/* общее колличество товаров */
+	 for (let j = 0; j < allCashbox.length; j++) {
+		num.push(allCashbox[j][0]);
+		items += num[j];                            // 43
+	 }
+/*  средняя стоимость одного товара */
+	sum = Math.round(sum / items);
 	return sum;
 }
 let res = getAveragePriceGoods(array); 
-console.log('Средняя стоимость одного товара:', res); // 4087.5
+console.log('Средняя стоимость одного товара:', res); // 5536
