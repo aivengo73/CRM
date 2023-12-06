1dc1fb838de46c7265db192294b479e4f4d04d09
Андрей Огнев <99536610+aivengo73@users.noreply.github.com>
2023-12-06 18:33:20 +0300

    Исправил задание 2 в модуле 4.9

--- js/lesson09/script1.js
+++ js/lesson09/script1.js
@@ -3,10 +3,16 @@
 import {listIPv4} from './modules/ipv4.js';
 
 function getUniqueIpAddresses() {
+	let array = [];
+	let num = 0;
 	const UniqueIpAddresses = new Set(listIPv4);
-
-	return [...new Set(UniqueIpAddresses)];
+	array = [...new Set(UniqueIpAddresses)];
+	return num = array.length
 }
 
 let res = getUniqueIpAddresses(listIPv4);
+
 console.log('res:', res);
+
+
+
